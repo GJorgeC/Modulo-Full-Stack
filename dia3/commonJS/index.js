d030f3c8d96606721f35e821bdb7e36603f66935
Ramón Vargas Ruiz <r.m.vargasruiz97@alumnos.cei.es>
2025-04-24 21:11:07 +0200

    Primer commit dia 24/4

---
+++ dia3/commonJS/index.js
@@ -0,0 +1,5 @@
+// importar un modulo
+const math = require('./math.js'); // Lo estamos usando como un objeto 
+
+console.log('La suma de los dos números introducidos es -> ' + math.sum(5,3));
+console.log('La resta de los dos números introducidos es -> ' + math.subtract(5,3));
